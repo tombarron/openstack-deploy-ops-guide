f131623214829b367dfb5c709104fd5f193d8dc3
Bob Callaway <bob.callaway@netapp.com>
2014-06-03 10:00:44 -0400

    dynamic sidebar 2

--- javascript/payload.js
+++ javascript/payload.js
@@ -26,7 +26,7 @@ $.ajax({
             var releases;
             for (var i = 0; i < tagArray.length; i++ ) {
                 var displayName = tagArray[i].charAt(0).toUpperCase() + tagArray[i].slice(1);
-                releases += "<li><a href=\"" + baseUrl  + tagArray[i] + '/' +  "\">" + displayName + "</a></li>";
+                releases += "<li><i class=\"fa fa-share\"></i><a href=\"" + baseUrl  + tagArray[i] + '/' +  "\">" + displayName + "</a></li>";
             }
             document.getElementById("releaseList").innerHTML = releases;
         }
