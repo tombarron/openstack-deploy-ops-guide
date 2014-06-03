18101d3532fb2e50c61a82fe1a6d0f41b620d747
Bob Callaway <bob.callaway@netapp.com>
2014-06-02 20:27:44 -0400

    attempt list by tag

--- javascript/payload.js
+++ javascript/payload.js
@@ -1,54 +1,24 @@
 // Milestones
 // Fetch date and title of last milestone closed
 
-var month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 $.ajax({
-    url: "https://api.github.com/repos/softlayer/jumpgate/milestones?state=closed/callback?", 
+    url: "https://api.github.com/repos/netapp/openstack-deploy-ops-guide/tags?", 
     dataType: 'jsonp',
     success: function (json) {
-        var lastMilestone = json.data[0];
+        var tagArray;
+        var k;
+        for (k = 0; k < json.length; ++k) 
+        { 
+            tagArray.push(json[k].name);
+        }
+        if (tagArray.length > 0)
+        {
+            tagArray.sort();
+            tagArray.reverse();
 
-        var stamp = new Date(lastMilestone.updated_at),
-            stampString = month[stamp.getMonth()] + ' ' + stamp.getDate();
-        $('#mdate').text(stampString);
-        $('#mtitle').text(lastMilestone.title);
+            $('mtitle').text(tagArray[0].charAt(0).toUpperCase() + tagArray[0].slice(1));
+            var currentVal = $('latestLink').text();
+            $('latestLink').text(currentVal + tagArray[0] + '/');
+        }
     }
-});
-
-// Commitments
-// Fetch date for last commit
-
-var month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-$.ajax({
-    url: "https://api.github.com/repos/softlayer/jumpgate/commits?state=closed/callback?", 
-    dataType: 'jsonp',
-    success: function (json) {
-        var lastCommit = json.data[0];
-
-        var stamp = new Date(lastCommit.commit.committer.date),
-            stampString = month[stamp.getMonth()] + ' ' + stamp.getDate();
-        $('#cdate').text(stampString);
-    }
-});
-
-// Team/Contributors
-// Fetch # of contributors
-
-$.getJSON("https://api.github.com/repos/softlayer/jumpgate/contributors?callback=?", function (result) { 
-    var tcount = result.data;
-
-    $(function () {
-        $("#tcount").text(tcount.length);
-    });
-});
-
-// Repositories
-// Fetch # of repos
-
-$.getJSON("https://api.github.com/orgs/jumpgate/repos?callback=?", function (result) { 
-    var rcount = result.data;
-
-    $(function () {
-        $("#rcount").text(rcount.length);
-    });
 });
